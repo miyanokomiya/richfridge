bdc4e0d6d8c40a4123dcacb67fdd7871e0f342d0
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-03-24 19:45:15 +0900

    動いたっぽい作業ログ

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM node:8.15.1-alpine
+WORKDIR /app
+ENV HOST 0.0.0.0
+EXPOSE 3000
---
+++ components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop({ type: String, default: 'Hello' })
+  message: string
+}
+</script>
