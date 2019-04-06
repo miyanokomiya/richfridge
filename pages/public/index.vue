0298273d8d4193fe68d7bf2749de4c360675202f
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-04-06 18:17:34 +0900

    メッセージ調整

--- pages/public/index.vue
+++ pages/public/index.vue
@@ -2,7 +2,7 @@
   <section class="container h-full p-2">
     <div>
       <p>
-        この画面から作成したデータは保護されず、URLを知っている誰もが閲覧・編集可能です。
+        この画面から作成したデータは保護されず、URLを知っている誰でも閲覧・編集可能です。
       </p>
       <p>URLの再発行はできません。紛失にご注意ください。</p>
     </div>
@@ -13,7 +13,7 @@
     </div>
     <div class="text-center my-2">
       <nuxt-link to="/" class="text-sm">
-        保護されたデータを作成する
+        保護されたデータを作成する<br />(要認証)
       </nuxt-link>
     </div>
   </section>
